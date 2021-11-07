6a534e54c6a19029306e68242833692f435711e9
johnTran <mrjohntran123@gmail.com>
2021-11-07 01:00:41 -0700

    can create new customer

--- client/src/screens/Orders.jsx
+++ client/src/screens/Orders.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { useEffect, useState } from "react";
-import { getAllCustomers } from '../services';
+import { getAllCustomers, createCustomer } from '../services';
+import { useHistory } from 'react-router';
 
 
 
 const Orders = (props) => {
     const [customers, setAllCustomers] = useState([]);
+    const [newCustomer, setNewCustomer] = useState('')
+    const history = useHistory();
 
     useEffect(() => {
 
         getAllCustomers().then((fetchedCustomers) => setAllCustomers(fetchedCustomers));
     }, [])
-    console.log(customers)
+   
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const addCustomer = {
+            customer_name: newCustomer,
+        }
+        await createCustomer(addCustomer)
+        history.push("/boards");
+    }
     return (
         <div>
+               <form onSubmit={handleSubmit}>
+            <label htmlFor="customer">Customer:</label>
+            <input
+                id="customer"
+                type="text"
+                value={newCustomer}
+                onChange={(e) => setNewCustomer(e.target.value)}
+                required
+            />
+            <button type="submit">Create Customer</button>
+        </form>
             <section>
-            <h1>Hello</h1>
+            <h1>Customer Name:</h1>
             {customers.map((customer, index) => (
                 <div key={index}>
-                    <h3>{customer.name}</h3>
+                    <h3>{customer.customer_name}</h3>
             
                 </div>
             ))}
